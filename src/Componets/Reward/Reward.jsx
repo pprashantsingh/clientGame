6ff75206572f17bdb4d00cd652b79f8497037302
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-01-26 23:25:54 +0530

    .

---
+++ src/Componets/Reward/Reward.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Reward=()=>{
+    return(
+        <>
+        <div className="Reward_cantainer">
+            <div className="first_deposit"></div>
+            <div className="vip_reward"></div>
+            <div className="Real_time"></div>
+        </div>
+        </>
+    )
+}
+export default Reward;
