b63ad57838dbdc45d5f74331f94df9ebc89e74fb
Prashant Singh <pprashantsingh2002@gmail.com>
2025-01-26 20:51:20 +0530

    commit images

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import logo from './images/logobg 1.png'
+import playicon from './images/Vector (1).png'
+import girl from './images/Group 5.png'
+import playstore from './images/Mask group.png'
+import applestore from './images/Vector.png'
+
+const images={
+    logo,
+    playicon,
+    girl,
+    playstore,
+    applestore
+}
+
+export default images;
