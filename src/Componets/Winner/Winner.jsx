9a488b1a138c63990b52e93174a968f7190757d6
Vijay Sharma <vijaysharma@mysita.com>
2025-01-27 12:30:01 +0530

    winner component

---
+++ src/Componets/Winner/Winner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Winner.css";
+
+const Winner=()=>{
+    return(
+        <>
+        <div className="Winner_Wrapper">
+
+        </div>
+        </>
+    )
+}
+export default Winner;
