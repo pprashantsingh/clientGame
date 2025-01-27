542aa020dc6c6b6411177c4bc68567536ff7c863
prashant <prashant.singh@soolegal.co.in>
2025-01-27 14:41:53 +0530

    paymentpartner with responsive is done

--- src/Componets/Paymentpartner/Paymentpartner.js
+++ src/Componets/Paymentpartner/Paymentpartner.js
@@ -14,18 +14,24 @@ const Paymentpartner = () => {
                     <p className='pay_sub_para1'>Players must be minimum 18 yeers of age to play
                         rcal moncy rummy gomes This gome may be
                         habit-forming or financially risky Play responsibly.</p>
+                    <div className='pay_horizonatal_style'>
+                        <hr></hr>
+                    </div>
                 </div>
 
-                <div className='payment_subconatiner1'>
+                <div className='payment_subconatiner3'>
                     <div className='pay_img1'>
                         <img src={images.paymenticon2} alt='payment_img2' className='payment_img' />
                     </div>
                     <p className='pay_para1'>Secured Payment Partner</p>
                     <div className='pay_second_col'>
-                        <img src={images.paytm} alt='paytm' className='pay_paytm'/>
-                        <img src={images.visa} alt='visa' className='pay_visa'/>
-                        <img src={images.phonepay} alt='Ppay' className='pay_phone'/>
-                        <img src={images.googlepay} alt='gPay' className='pay_google'/>
+                        <img src={images.paytm} alt='paytm' className='pay_paytm' />
+                        <img src={images.visa} alt='visa' className='pay_visa' />
+                        <img src={images.phonepay} alt='Ppay' className='pay_phone' />
+                        <img src={images.googlepay} alt='gPay' className='pay_google' />
+                    </div>
+                    <div className='pay_horizonatal_style'>
+                        <hr></hr>
                     </div>
                 </div>
 
