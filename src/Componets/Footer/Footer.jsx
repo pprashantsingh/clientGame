d5e3c35453b2570d672e5fed9d0daf75b50a88ce
prashant <prashant.singh@soolegal.co.in>
2025-01-29 15:44:44 +0530

    changes

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -15,48 +15,53 @@ const Footer = () => {
                                 <p>ROCKET <span>BET</span></p>
                             </div>
                         </div>
-                        <p className="support_team_title">
-                            24/7 Support Team
-                        </p>
 
-                        <div className="_footer_mail_container">
-                            <div className="_mail_image_container">
-                                <img src={images?.mail} alt="" />
-                            </div>
-                            <div className="_mail_title_container">
-                                <p className="_mail_title">
-                                    therocketbet@gmail.com
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="_footer_location_container">
-                            <div className="_mail_image_container">
-                                <img src={images?.location} alt="" />
-                            </div>
-                            <div className="_mail_title_container">
-                                <p className="_mail_title">
-                                    No:30, 1st street ,Patel Nagar ND - 08
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="_footer_availabel_on">
-                            <p className="availabel_on_title">
-                                Instant Download Here
+                        <div className='showhide_footer_dowloand1'>
+                            <p className="support_team_title">
+                                24/7 Support Team
                             </p>
 
-                            <div className="_availabel_on_image_container">
-                                <div className="_image_container_1">
-                                    <img src={images?.playstores} alt="" />
+                            <div className="_footer_mail_container">
+                                <div className="_mail_image_container">
+                                    <img src={images?.mail} alt="" />
                                 </div>
-                                <div className="_image_container_1">
-                                    <img src={images?.appStore} alt="" />
+                                <div className="_mail_title_container">
+                                    <p className="_mail_title">
+                                        therocketbet@gmail.com
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="_footer_location_container">
+                                <div className="_mail_image_container">
+                                    <img src={images?.location} alt="" />
+                                </div>
+                                <div className="_mail_title_container">
+                                    <p className="_mail_title">
+                                        No:30, 1st street ,Patel Nagar ND - 08
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='showhide_footer_dowloand1'>
+                            <div className="_footer_availabel_on">
+                                <p className="availabel_on_title">
+                                    Instant Download Here
+                                </p>
+
+                                <div className="_availabel_on_image_container">
+                                    <div className="_image_container_1">
+                                        <img src={images?.playstores} alt="" />
+                                    </div>
+                                    <div className="_image_container_1">
+                                        <img src={images?.appStore} alt="" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
+                    {/* <div className='footer_wrapper_1'> */}
                     <div className="_footer_container_row_col">
                         <p className="_footer_title_container">
                             World Class Security
@@ -86,18 +91,19 @@ const Footer = () => {
                                 Verified RNG
                             </p>
                         </div>
+                        <div className='showhide_footercontent1'>
+                            <div className="_footer_col_media_to_follow">
+                                <p className="_footer_spade_title">
+                                    Follow Us On
+                                </p>
 
-                        <div className="_footer_col_media_to_follow">
-                            <p className="_footer_spade_title">
-                                Follow Us On
-                            </p>
+                                <div className="_follow_us_on_media_image_container">
+                                    <img src={images?.facebook} alt="" />
+                                    <img src={images?.insta} alt="" />
+                                    <img src={images?.twitter} alt="" />
+                                    <img src={images?.telegram} alt="" />
 
-                            <div className="_follow_us_on_media_image_container">
-                                <img src={images?.facebook} alt="" />
-                                <img src={images?.insta} alt="" />
-                                <img src={images?.twitter} alt="" />
-                                <img src={images?.telegram} alt="" />
-
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -133,7 +139,7 @@ const Footer = () => {
                             </p>
                         </div>
                     </div>
-
+                    {/* </div> */}
 
                     <div className="_footer_container_row_col">
                         <p className="_footer_title_container">
@@ -156,14 +162,74 @@ const Footer = () => {
                                 Our Payment Partners
                             </p>
                             <div className="_game_integraty_container">
-                            <img src={images?.googlepay} alt="" />
-                            <img src={images?.paytm} alt="" />
-                            <img src={images?.phonepay} alt="" />
-                            <img src={images?.amazonPay} alt="" />
-                           
+                                <img src={images?.googlepay} alt="" />
+                                <img src={images?.paytm} alt="" />
+                                <img src={images?.phonepay} alt="" />
+                                <img src={images?.amazonPay} alt="" />
 
+
+                            </div>
+
+                            <div className='showhide_footer_dowloand'>
+                            <p className="support_team_title">
+                                24/7 Support Team
+                            </p>
+
+                            <div className="_footer_mail_container">
+                                <div className="_mail_image_container">
+                                    <img src={images?.mail} alt="" />
+                                </div>
+                                <div className="_mail_title_container">
+                                    <p className="_mail_title">
+                                        therocketbet@gmail.com
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="_footer_location_container">
+                                <div className="_mail_image_container">
+                                    <img src={images?.location} alt="" />
+                                </div>
+                                <div className="_mail_title_container">
+                                    <p className="_mail_title">
+                                        No:30, 1st street ,Patel Nagar ND - 08
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        <p className="_footer_title_all">
+
+                            <div className='showhide_footer_dowloand'>
+                                <div className="_footer_availabel_on">
+                                    <p className="availabel_on_title">
+                                        Instant Download Here
+                                    </p>
+
+                                    <div className="_availabel_on_image_container">
+                                        <div className="_image_container_1">
+                                            <img src={images?.playstores} alt="" />
+                                        </div>
+                                        <div className="_image_container_1">
+                                            <img src={images?.appStore} alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='showhide_footercontent'>
+                                <div className="_footer_col_media_to_follow">
+                                    <p className="_footer_spade_title">
+                                        Follow Us On
+                                    </p>
+
+                                    <div className="_follow_us_on_media_image_container">
+                                        <img src={images?.facebook} alt="" />
+                                        <img src={images?.insta} alt="" />
+                                        <img src={images?.twitter} alt="" />
+                                        <img src={images?.telegram} alt="" />
+
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="_footer_title_all">
                                 @All Rights Reserved - 2021 - 2025 |<span> Rocket Bet</span>
                             </p>
                         </div>
