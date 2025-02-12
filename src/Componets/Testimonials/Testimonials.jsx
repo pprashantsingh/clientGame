2e6386ebd51240089ef35b9d69c5a978e5ed14b9
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-02-12 22:51:53 +0530

    solve the testimonials timer problem

--- src/Componets/Testimonials/Testimonials.jsx
+++ src/Componets/Testimonials/Testimonials.jsx
@@ -1,50 +1,54 @@
-import React from 'react';
+import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import 'swiper/css/autoplay';
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 import "./testimonials.css";
-import images from '../../assets';
+import images from "../../assets";
 
 const Testimonials = () => {
   return (
     <>
       <div className="_testimonials_btn_container">
-         <button className='testimonials_btn winner-heading'>
+        <button className="testimonials_btn winner-heading">
           Testimonials
-         </button>
+        </button>
       </div>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        initialSlide={1} // Set the default slide index to 1 (second slide)
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src={images?.Aditya} alt="Aditya" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={images?.Nisha} alt="Nisha" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={images?.Raj} alt="Raj" />
-        </SwiperSlide>
-
-      </Swiper>
+      <Swiper  
+  effect={'coverflow'}
+  grabCursor={true}
+  centeredSlides={true}
+  slidesPerView={'auto'}
+  initialSlide={1} // Set the default slide index to 1 (second slide)
+  coverflowEffect={{
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  }}
+  pagination={{
+    clickable: true,
+  }}
+  autoplay={{
+    delay: 2500, // Time in ms before the next slide
+    disableOnInteraction: false, // Keep autoplay running even after user interaction
+  }}
+  modules={[EffectCoverflow, Pagination, Autoplay]}
+  className="mySwiper"
+>
+  <SwiperSlide>
+    <img src={images?.Aditya} alt="Aditya" />
+  </SwiperSlide>
+  <SwiperSlide>
+    <img src={images?.Nisha} alt="Nisha" />
+  </SwiperSlide>
+  <SwiperSlide>
+    <img src={images?.Raj} alt="Raj" />
+  </SwiperSlide>
+</Swiper>
     </>
   );
 };
