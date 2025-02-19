dbfe109af3f09747661091a331079362754113bf
Vijay Sharma <vijaysharma@mysita.com>
2025-02-19 11:55:22 +0530

    router

--- src/Componets/RouterLink.jsx
+++ src/Componets/RouterLink.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes,Router, Route } from "react-router-dom";
 import PrivacyPolicy from "./policy/PrivacyPolicy";
 import Cookie from "./cookies/Cookie";
 import Refund from "./RefundCancel/Refund";
@@ -8,13 +8,14 @@ import Term from "./term/Term";
 
 const RouterLink = () => {
   <>
-
-      <Routes>
+   <Router>
+      <Routes>  
         <Route path="/privacy-policy" element={<PrivacyPolicy/>} />
         <Route path="/cookie-policy" element={<Cookie/>} />
         <Route path="/refund-policy" element={<Refund/>} />
         <Route path="/terms-and-conditions" element={<Term/>} />
       </Routes>
+      </Router>
 
   </>;
 };
